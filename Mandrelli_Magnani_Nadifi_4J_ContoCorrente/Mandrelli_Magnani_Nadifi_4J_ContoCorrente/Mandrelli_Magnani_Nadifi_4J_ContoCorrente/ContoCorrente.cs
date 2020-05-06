02962d3daf16430582e43f7e6b97e2f09a2ccd3e
Francesco Mandrelli <46867091+Radicaal26@users.noreply.github.com>
2020-05-06 11:53:06 +0200

    Aggiunta del file finale

---
+++ Mandrelli_Magnani_Nadifi_4J_ContoCorrente/Mandrelli_Magnani_Nadifi_4J_ContoCorrente/Mandrelli_Magnani_Nadifi_4J_ContoCorrente/ContoCorrente.cs
@@ -0,0 +1,332 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mandrelli_Magnani_Nadifi_4J_ContoCorrente
+{
+    class ContoCorrente//Dichiarazione classe Conto Corrente
+    {
+        string iban;
+        protected float saldo;
+        Banca banca;
+        Intestatario intestatario;
+        ContoOnline contoOnline;
+        Bonifico bonifico;
+        protected List<Movimento> movimenti = new List<Movimento>(); //Lista di movimenti
+
+        public ContoCorrente( string iban, float saldo)//Costruttore classe
+        {
+            this.iban = iban;
+            this.saldo = saldo;
+        }
+
+        public string getNomeintestatario()//Metodo che restituisce nome intestatario
+        {
+            return intestatario.getNome();
+        }
+
+        public string getIban()//Metodo che restituisce Iban
+        {
+            return iban;
+        }
+
+        public float getSaldo()//Metodo che restituisce saldo
+        {
+            return saldo;
+        }
+
+        public virtual void setSaldo(float importo) //Aggiorna il saldo del conto
+        {
+            saldo = saldo + importo;
+        }
+
+        public virtual void AddMovimenti(Movimento movimento)//Viene aggiunto un movimento alla lista dei movimenti
+        {
+            movimenti.Add(movimento);
+        }
+
+        public virtual List<Movimento> getMovimenti()
+        {
+            return movimenti;
+        }
+
+        public virtual int getNumeroMovimenti()
+        {
+            return movimenti.Count();
+        }
+    }
+
+    class Intestatario
+    {
+        string nome;
+        string cognome;
+        DateTime dataNascita;
+        string codiceFiscale;
+        string indirizzo;
+        string telefono;
+        ContoCorrente contoCorrente;
+
+        public Intestatario(string nome, string cognome, DateTime dataNascita, string codiceFiscale, string indirizzo, string telefono)
+        {
+            this.nome = nome;
+            this.cognome = cognome;
+            this.dataNascita = dataNascita;
+            this.codiceFiscale = codiceFiscale;
+            this.indirizzo = indirizzo;
+            this.telefono = telefono;
+        }
+
+        public string getNome()
+        {
+            return nome;
+        }
+
+        public string getCognome()
+        {
+            return cognome;
+        }
+
+        public DateTime getDataNascita()
+        {
+            return dataNascita;
+        }
+
+        public string getCodiceFiscale()
+        {
+            return codiceFiscale;
+        }
+
+        public string getIndirizzo()
+        {
+            return indirizzo;
+        }
+
+        public string getTelefono()
+        {
+            return telefono;
+        }
+    }
+
+    class Bonifico : Movimento
+    {
+        string mittente;
+        float commissione;
+
+        ContoCorrente contoCorrente1;
+
+        public Bonifico(ContoCorrente contoCorrente1, float commissione, float importo, DateTime dataOra, ContoCorrente contoCorrente, ContoOnline contoOnline) : base (importo, dataOra, contoCorrente, contoOnline)
+        {
+            this.contoCorrente1 = contoCorrente1;
+            this.commissione = commissione;
+        }
+
+        public void EseguiBonifico()//Metodo che ci permette di eseguire un bonifico, andando a prelevare i soldi da un conto per versarli in quello del destinatario
+        {
+            contoCorrente1.setSaldo(importo);
+            importo = -importo;
+            contoCorrente.setSaldo(importo);
+
+        }
+
+        public string getMittente()//Restituisce il mittente del bonifico
+        {
+            return contoCorrente1.getNomeintestatario();
+        }
+
+        public float getCommissione()
+        {
+            return commissione;
+        }
+    }
+
+    class ContoOnline : ContoCorrente
+    {
+        float maxPrelievo;
+        string username;
+        string password;
+        
+        public ContoOnline(string username, string password, string iban, float saldo) : base(iban, saldo)
+        {
+            maxPrelievo = 3000;
+            this.username = username;
+            this.password = password;
+        }
+
+        public override void setSaldo(float importo)
+        {
+            if (importo > 0 || (importo < 0 && importo >= -maxPrelievo))
+            {
+                saldo = saldo + importo;
+            }
+        }
+
+        public string getUsername()
+        {
+            return username;
+        }
+
+        public string getPassword()
+        {
+            return password;
+        }
+
+        public override void AddMovimenti(Movimento movimento)
+        {
+            movimenti.Add(movimento);
+        }
+
+        public override List<Movimento> getMovimenti()
+        {
+            return movimenti;
+        }
+
+        public override int getNumeroMovimenti()
+        {
+            return movimenti.Count();
+        }
+
+    }
+
+    class Banca
+    {
+        string nome;
+        string indirizzo;
+        ContoCorrente ContoCorrente;
+        List<ContoCorrente> conti = new List<ContoCorrente>(); //Lista di conti 
+
+        public Banca(string nome, string indirizzo)
+        {
+            this.nome = nome;
+            this.indirizzo = indirizzo;
+        }
+
+        public string getNome()
+        {
+            return nome;
+        }
+
+        public string getIndirizzo()
+        {
+            return indirizzo;
+        }
+
+        public void AddConto (ContoCorrente c)//Viene aggiunto un conto alla lista dei conti
+        {
+            conti.Add(c);
+        }
+    }
+
+    class Movimento
+    {
+        protected ContoCorrente contoCorrente;
+        protected ContoOnline contoOnline;
+        int nMovimenti;
+        int maxMovimenti;
+        protected float importo;
+        protected DateTime dataOra;
+
+        public Movimento(float importo, DateTime dataOra, ContoCorrente contoCorrente, ContoOnline contoOnline)
+        {
+            this.importo = importo;
+            this.dataOra = dataOra;
+            this.contoCorrente = contoCorrente;
+            this.contoOnline = contoOnline;
+            if (contoCorrente == null && contoOnline != null)
+            {
+                nMovimenti = contoOnline.getNumeroMovimenti(); //Restituisce il numero di movimenti del conto online
+            }
+            else
+            {
+                if (contoOnline == null && contoCorrente != null)
+                {
+                    nMovimenti = contoCorrente.getNumeroMovimenti(); //Restituisce il numero di movimenti del conto corrente
+                }
+                else
+                {
+                    nMovimenti = 0;
+                }
+            }
+      
+            maxMovimenti = 50;
+        }
+
+        public virtual void Sommare()
+        {
+            contoCorrente.setSaldo(importo);//Importo movimento nel conto corrente
+        }
+
+        public virtual void SommareOnline()
+        {
+            contoOnline.setSaldo(importo);//Importo movimento nel conto online
+        }
+
+        public virtual float getImporto()//Restituisce l'importo del movimento
+        {
+            return importo;
+        }
+
+        public virtual DateTime getDataOra()//Restituisce l'orario del movimento
+        {
+            return dataOra;
+        }
+    }
+
+    class Prelievo : Movimento
+    {      
+        public Prelievo(float importo, DateTime dataOra, ContoCorrente contoCorrente, ContoOnline contoOnline) : base(importo, dataOra, contoCorrente, contoOnline)
+        {
+            
+        }
+
+        public override void Sommare()//Effettua un prelievo dal conto corrente
+        {
+            importo = -importo;
+            contoCorrente.setSaldo(importo);
+        }
+
+        public override void SommareOnline()//Effettua un prelievo dal conto online
+        {
+            importo = -importo;
+            contoOnline.setSaldo(importo);
+        }
+
+        public override float getImporto()//Restituisce l'importo del prelieve
+        {
+            return importo;
+        }
+
+        public override DateTime getDataOra()//Restituisce l'orario del versamento
+        {
+            return dataOra;
+        }
+    }
+
+    class Versamento : Movimento
+    {
+        public Versamento(float importo, DateTime dataOra, ContoCorrente contoCorrente, ContoOnline contoOnline) : base(importo, dataOra, contoCorrente, contoOnline)
+        {
+            
+        }
+
+        public override void Sommare()//Importo versamento nel conto corrente
+        {
+            contoCorrente.setSaldo(importo);
+        }
+
+        public override void SommareOnline()//Importo versamento nel conto online
+        {
+            contoOnline.setSaldo(importo);
+        }
+
+        public override float getImporto()//Restituisce l'importo del versamento
+        {
+            return importo;
+        }
+
+        public override DateTime getDataOra()//Restituisce l'orario del versamento
+        {
+            return dataOra;
+        }
+    }
+}
---
+++ Mandrelli_Magnani_Nadifi_4J_ContoCorrente/Mandrelli_Magnani_Nadifi_4J_ContoCorrente/Mandrelli_Magnani_Nadifi_4J_ContoCorrente/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mandrelli_Magnani_Nadifi_4J_ContoCorrente
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creazione di una banca
+            Banca DB = new Banca("Deutsche Bank", "viale Principe Amedeo");
+
+            // Creazione del primo intestatario e del suo relativo conto corrente (tradizionale)
+            Intestatario A = new Intestatario("Mario", "Rossi", new DateTime(1990 , 06 , 19), "weudu8763", "via Roma", "3874636784");
+            ContoCorrente AC = new ContoCorrente("IT00000001", 5000);
+            // Aggiungiamo il conto alla lista di conti
+            DB.AddConto(AC);
+            // Stampa info intestatario e del relativo conto 
+            Console.WriteLine("Intestatario: " + A.getNome() + " " + A.getCognome() + "\nData di nascita: " + A.getDataNascita() + "\nAbita in via: " + A.getIndirizzo() + "\nNumero di telefono: " + A.getTelefono());
+            Console.WriteLine("Stampa del saldo di " + A.getNome() + " " + A.getCognome() + ": " + AC.getSaldo());
+            // Vengono effettuati un versamento e un prelievo con la relativa stampa aggiornata
+            AC.AddMovimenti(new Versamento(100, new DateTime(2020, 03, 19), AC, null));
+            AC.AddMovimenti(new Prelievo(300, new DateTime(2020, 04, 10), AC, null));
+            Console.WriteLine("MOVIMENTI:");
+            foreach (Movimento movimento in AC.getMovimenti())
+            {               
+                movimento.Sommare();
+                Console.Write(movimento.getImporto() + " euro" + " fatto nel: " + movimento.getDataOra() + " - saldo attuale: " + AC.getSaldo() + "\n");
+            }
+            Console.WriteLine("__________________________________________________________________________________________________\n");
+
+
+            // Creazione del secondo intestatario
+            Intestatario B = new Intestatario("Luigi", "Verdi", new DateTime(1990, 04, 26), "asdaf5678", "via Roma", "3981848734");
+            // Creazione del suo conto corrente (tradizionale)
+            ContoCorrente BC = new ContoCorrente("IT00000002", 2500);
+            // Aggiunta del conto corrente alla lista di conti
+            DB.AddConto(BC);
+            // Stampa delle info del proprietario e del relativo conto
+            Console.WriteLine("Intestatario: " + B.getNome() + " " + B.getCognome() + "\nData di nascita: " + B.getDataNascita() + "\nAbita in via: " + B.getIndirizzo() + "\nNumero di telefono: " + B.getTelefono());
+            Console.WriteLine("Stampa del saldo di " + B.getNome() + " " + B.getCognome() + ": " + BC.getSaldo());
+            // Vengono effettutati dei movimenti di prova come il versamento, il prelievo e il bonifico
+            BC.AddMovimenti(new Versamento(300, new DateTime(2020, 05, 30), BC, null));
+            BC.AddMovimenti(new Prelievo(100, new DateTime(2020, 07, 21), BC, null));
+            BC.AddMovimenti(new Bonifico(AC, 50, -200, new DateTime(2020, 06, 06), BC, null));
+            // Stampa dei movimenti effettuati
+            Console.WriteLine("MOVIMENTI:");
+            foreach (Movimento movimento in BC.getMovimenti())
+            {
+                movimento.Sommare();               
+                Console.Write(movimento.getImporto() + " euro " + "fatto nel: " + movimento.getDataOra() + " - saldo attuale: " + BC.getSaldo() + "\n");
+            }
+            Console.WriteLine("__________________________________________________________________________________________________\n");
+
+            //Creazione di un nuovo intestatario online e del suo relativo conto online, aggiungendolo alla lista dei conti
+            Intestatario C = new Intestatario("Antonio", "Peroni", new DateTime(1968, 09, 09), "udhwoiud83", "via Lazio", "38648374683");
+            ContoOnline CC = new ContoOnline("Anto384", "vero4666", "IT00000003", 10000);
+            DB.AddConto(CC);
+            Console.WriteLine("Intestatario: " + C.getNome() + " " + C.getCognome() + "\nData di nascita: " + C.getDataNascita() + "\nAbita in via: " + C.getIndirizzo() + "\nNumero di telefono: " + C.getTelefono());
+            Console.WriteLine("Stampa del saldo di " + C.getNome() + " " + C.getCognome() + ": " + CC.getSaldo());
+            //Vengono effettuati dei movimenti, aggiunti poi alla lista dei movimenti
+            CC.AddMovimenti(new Versamento(1000, new DateTime(2020, 01, 07), null, CC));
+            CC.AddMovimenti(new Prelievo(600, new DateTime(2020, 09, 25), null, CC));
+            // Stampa dei movimenti effettuati
+            Console.WriteLine("MOVIMENTI:");
+            foreach (Movimento movimento in CC.getMovimenti())
+            {              
+                movimento.SommareOnline();
+                Console.Write(movimento.getImporto() + " euro " + "fatto nel: " + movimento.getDataOra() + " - saldo: " + CC.getSaldo() + "\n");
+            }
+            Console.WriteLine("__________________________________________________________________________________________________\n");
+
+            Console.ReadLine();
+        }
+    }
+}
